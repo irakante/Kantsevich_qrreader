fcce57bb90e5e690b9fdb054eb9f9deff159d46f
Ира Ира <irakante@gmail.com>
2019-01-26 16:20:30 +0200

    Add project files.

---
+++ Kantsevich_qrreader/Kantsevich_qrreader/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Kantsevich_qrreader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+            await Navigation.PushAsync(scanPage);
+            scanPage.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+               {
+                   await Navigation.PopAsync();
+                   CodeResult.Text = result.Text;
+
+                   if (Uri.IsWellFormedUriString(result.Text, UriKind.Absolute))
+                   {
+                       //adding clickability to label
+                       CodeResult.GestureRecognizers.Add(
+                        new TapGestureRecognizer()
+                        {
+                            Command = new Command(async () =>
+                            {
+                                await Task.Run(() => { Device.OpenUri(new Uri(result.Text)); });
+                            })
+                        }
+                    );
+                   }
+
+                     
+               });
+            };
+        }
+    }
+}
